1217193c4149cfeb1616dbf1a936a8e958929e5c
AlexKozh <kojemiako-sascha@yandex.ru>
2022-12-15 20:39:20 +0700

    Solved task 3

--- HW_2/task_3.py
+++ HW_2/task_3.py
@@ -16,3 +16,19 @@
 3
 6.62
 """
+
+
+n = int(input("Enter the number N: "))
+if n > 0:
+    x = sum([(1 + 1 / i) ** i for i in range(1, n + 1)])
+    # i = 0
+    # x = 0
+    # list = []
+    # while i <= n - 1:
+    #     i = i + 1
+    #     b = (1 + (1 / i)) ** i
+    #     list.append(b)
+    #     x = x + b
+    print(round(x, 2))
+else:
+    print('Input Error')
