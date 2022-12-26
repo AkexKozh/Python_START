72d08e6dadcb89cb3097bc671aa57d7774bd41a0
AlexKozh <kojemiako-sascha@yandex.ru>
2022-12-26 12:10:06 +0700

    Solved task_1 and Task_2

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -4,3 +4,16 @@
 Ввод: значение типа <str>
 Вывод: значение типа <str>
 """
+
+
+
+str_text = "арбуз. дома уютно. новый год. автомобиль."
+str_num = []
+
+value = str_text.split()
+for i in value:
+    if not ("а" in i and "б" in i and "в" in i):
+        str_num.append(i)
+
+result = " ".join(str_num)
+print(f"Слова, не содержащие в себе буквы а,б,в: {result}")
